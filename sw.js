057b9b8f70667b7d3db380146e6fd2893d3f957e
vvlad5 <galko3.v@gmail.com>
2025-11-16 22:43:25 +0300

    debug

--- sw.js
+++ sw.js
@@ -29,6 +29,10 @@ if (!self.define) {
 }
 define(["./workbox-d84cbe57"], function (e) {
   "use strict";
+    self.addEventListener("fetch", (event) => {
+        // Полный pass-through, чтобы Safari НЕ отключал кеш
+        event.respondWith(fetch(event.request));
+    });
   self.addEventListener("message", (e) => {
     e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting();
   }),
