1c729166c26e0a3af857012275e16eb5b05121a2
vvlad5 <galko3.v@gmail.com>
2025-11-16 23:29:56 +0300

    debug

--- sw.js
+++ sw.js
@@ -1,36 +1,38 @@
 if (!self.define) {
-  let e,
-    s = {};
-  const a = (a, c) => (
-    (a = new URL(a + ".js", c).href),
-    s[a] ||
-      new Promise((s) => {
+    let e, a = {};
+    const s = (s, i) => (s = new URL(s + ".js", i).href, a[s] || new Promise((a => {
         if ("document" in self) {
-          const e = document.createElement("script");
-          (e.src = a), (e.onload = s), document.head.appendChild(e);
-        } else (e = a), importScripts(a), s();
-      }).then(() => {
-        let e = s[a];
-        if (!e) throw new Error(`Module ${a} didn’t register its module`);
-        return e;
-      })
-  );
-  self.define = (c, i) => {
-    const d =
-      e ||
-      ("document" in self ? document.currentScript.src : "") ||
-      location.href;
-    if (s[d]) return;
-    let n = {};
-    const r = (e) => a(e, d),
-      t = { module: { uri: d }, exports: n, require: r };
-    s[d] = Promise.all(c.map((e) => t[e] || r(e))).then((e) => (i(...e), n));
-  };
+            const e = document.createElement("script");
+            e.src = s, e.onload = a, document.head.appendChild(e)
+        } else e = s, importScripts(s), a()
+    })).then((() => {
+        let e = a[s];
+        if (!e) throw new Error(`Module ${s} didn’t register its module`);
+        return e
+    })));
+    self.define = (i, d) => {
+        const c = e || ("document" in self ? document.currentScript.src : "") || location.href;
+        if (a[c]) return;
+        let n = {};
+        const r = e => s(e, c), t = {module: {uri: c}, exports: n, require: r};
+        a[c] = Promise.all(i.map((e => t[e] || r(e)))).then((e => (d(...e), n)))
+    }
 }
-define(["./workbox-d84cbe57"], function (e) {
-  "use strict";
-  self.addEventListener("message", (e) => {
-    e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting();
-  }),
-    e.precacheAndRoute([], { ignoreURLParametersMatching: [/.*/] });
-});
+define(["./workbox-445a40b5"], (function (e) {
+    "use strict";
+    importScripts("fallback-28d0d403-fa27-44e6-a276-2526efad7dcb.js"), self.addEventListener("message", (e => {
+        e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting()
+    })), e.clientsClaim(), e.precacheAndRoute([], {ignoreURLParametersMatching: [/.*/]}), e.registerRoute(/\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i, new e.StaleWhileRevalidate({
+        cacheName: "static-image-assets",
+        plugins: [new e.ExpirationPlugin({
+            maxEntries: 64,
+            maxAgeSeconds: 604800
+        }), {handlerDidError: async ({request: e}) => self.fallback(e)}]
+    }), "GET"), e.registerRoute((() => !1), new e.NetworkOnly({
+        cacheName: "others",
+        plugins: [new e.ExpirationPlugin({
+            maxEntries: 32,
+            maxAgeSeconds: 86400
+        }), {handlerDidError: async ({request: e}) => self.fallback(e)}]
+    }), "GET")
+}));
