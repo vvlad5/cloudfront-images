31333086ee44ddb9d7dcac9a9899ad45b0163e27
vvlad5 <galko3.v@gmail.com>
2025-11-17 00:11:27 +0300

    debug

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
-if(!self.define){let e,s={};const i=(i,a)=>(i=new URL(i+".js",a).href,s[i]||new Promise((s=>{if("document"in self){const e=document.createElement("script");e.src=i,e.onload=s,document.head.appendChild(e)}else e=i,importScripts(i),s()})).then((()=>{let e=s[i];if(!e)throw new Error(`Module ${i} didn’t register its module`);return e})));self.define=(a,n)=>{const r=e||("document"in self?document.currentScript.src:"")||location.href;if(s[r])return;let t={};const c=e=>i(e,r),b={module:{uri:r},exports:t,require:c};s[r]=Promise.all(a.map((e=>b[e]||c(e)))).then((e=>(n(...e),t)))}}define(["./workbox-1de034db"],(function(e){"use strict";self.addEventListener("message",(e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()})),e.clientsClaim(),e.precache([]),e.cleanupOutdatedCaches(),e.registerRoute(/\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,new e.StaleWhileRevalidate({cacheName:"static-image-assets",plugins:[new e.ExpirationPlugin({maxEntries:64,maxAgeSeconds:604800}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute((()=>!1),new e.NetworkOnly({cacheName:"others",plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET")}));
+if (!self.define) {
+    let e, s = {};
+    const i = (i, a) => (i = new URL(i + ".js", a).href, s[i] || new Promise((s => {
+        if ("document" in self) {
+            const e = document.createElement("script");
+            e.src = i, e.onload = s, document.head.appendChild(e)
+        } else e = i, importScripts(i), s()
+    })).then((() => {
+        let e = s[i];
+        if (!e) throw new Error(`Module ${i} didn’t register its module`);
+        return e
+    })));
+    self.define = (a, n) => {
+        const r = e || ("document" in self ? document.currentScript.src : "") || location.href;
+        if (s[r]) return;
+        let t = {};
+        const c = e => i(e, r), b = {module: {uri: r}, exports: t, require: c};
+        s[r] = Promise.all(a.map((e => b[e] || c(e)))).then((e => (n(...e), t)))
+    }
+}
+define(["./workbox-1de034db"], (function (e) {
+    "use strict";
+    self.addEventListener("message", (e => {
+        e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting()
+    })), e.clientsClaim(), e.precache([], {ignoreURLParametersMatching: [/.*/]}), e.cleanupOutdatedCaches(), e.registerRoute(/\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i, new e.StaleWhileRevalidate({
+        cacheName: "static-image-assets",
+        plugins: [new e.ExpirationPlugin({
+            maxEntries: 64,
+            maxAgeSeconds: 604800
+        }), {handlerDidError: async ({request: e}) => self.fallback(e)}]
+    }), "GET"), e.registerRoute((() => !1), new e.NetworkOnly({
+        cacheName: "others",
+        plugins: [new e.ExpirationPlugin({
+            maxEntries: 32,
+            maxAgeSeconds: 86400
+        }), {handlerDidError: async ({request: e}) => self.fallback(e)}]
+    }), "GET")
+}));
